f293abf1cea731e48bfaabbd78fde4189e63b710
Abhinav Allam <abhinavall0123@gmail.com>
2025-07-23 13:47:58 -0700

    style: Apply ESLint auto-fixes for indentation

--- jest.config.js
+++ jest.config.js
@@ -1,32 +1,32 @@
 // jest.config.js
 
 module.exports = {
-    // Specifies the root directories Jest should scan for tests and modules
-    roots: ['<rootDir>/__tests__'],
+  // Specifies the root directories Jest should scan for tests and modules
+  roots: ['<rootDir>/__tests__'],
   
-    // Automatically clear mock calls and instances between every test
-    clearMocks: true,
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
   
-    // An array of file extensions your modules use
-    moduleFileExtensions: ['js', 'jsx'],
+  // An array of file extensions your modules use
+  moduleFileExtensions: ['js', 'jsx'],
   
-    // The test environment that will be used for testing
-    testEnvironment: 'jsdom',
+  // The test environment that will be used for testing
+  testEnvironment: 'jsdom',
   
-    // A map from regular expressions to module names or to arrays of module names
-    moduleNameMapper: {
-      '\\.(css)$': 'identity-obj-proxy',
-    },
+  // A map from regular expressions to module names or to arrays of module names
+  moduleNameMapper: {
+    '\\.(css)$': 'identity-obj-proxy',
+  },
   
-    // A preset that is used as a base for Jest's configuration
-    preset: 'jest',
+  // A preset that is used as a base for Jest's configuration
+  preset: 'jest',
   
-    // Transform files matching the regex with the provided transformer
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
+  // Transform files matching the regex with the provided transformer
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
   
-    // Indicates whether each individual test should be reported during the run
-    verbose: true,
-  };
+  // Indicates whether each individual test should be reported during the run
+  verbose: true,
+};
   
